c754b3c7b800aff033de2081b049729afbcc6b04
Uha <uhabattu@gmail.com>
2026-07-27 13:24:41 +0530

    Make CORS origin configurable via environment variable

--- server/server.js
+++ server/server.js
@@ -14,7 +14,9 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+}));
 app.use(express.json());
 app.use(logger);
 
